b6e6392e28788ef8e2a10a286aeadc55a6de824f
ShristiAnjana-bit <Shristip263@gmail.com>
2026-03-26 22:38:01 +0530

    Time: 0 ms (100%), Space: 43.1 MB (39.63%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int count =0;
+        int n= nums.length;
+
+        for(int i=0;i<n;i++){
+            //check current element with the NEXT element
+            //we use(i+1)%n to loop back to the start at the end
+            if(nums[i]>nums[(i+1)%n]){
+                count++;
+            }
+        }
+        //if there's more than one break,it's not sorted /rotated array
+        return count <=1;
+        
+    }
+}
