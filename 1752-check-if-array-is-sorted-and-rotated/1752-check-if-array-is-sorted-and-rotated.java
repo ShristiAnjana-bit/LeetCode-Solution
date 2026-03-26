092626f4ad25c59f9480c9798422a8c56efa3e5e
ShristiAnjana-bit <Shristip263@gmail.com>
2026-03-27 05:23:45 +0530

    Time: 0 ms (100%), Space: 43.1 MB (58.72%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -9,9 +9,11 @@ class Solution {
             if(nums[i]>nums[(i+1)%n]){
                 count++;
             }
+            //optimization:if we find more than 1 break fail easily
+            if(count>1) return false;
         }
         //if there's more than one break,it's not sorted /rotated array
-        return count <=1;
+        return true;
         
     }
 }
