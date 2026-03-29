317f258184c3830df608781328c48033776e3ee5
ShristiAnjana-bit <Shristip263@gmail.com>
2026-03-29 06:19:28 +0530

    Time: 0 ms (100%), Space: 61.2 MB (79.77%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,24 +1,30 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-      int n = nums.length;
-      k = k%n ;//always do this !if k=10 and n = 7...(10-7=3). you only rotate 3 times
+     int n= nums.length;
+     k=k%n; //the "Golden rule" for rotation 
 
-      //step 1: copy last k element into the temp array
-      int[] temp = new int[k];
-     for(int i =0; i<k; i++){
-        temp[i] = nums[n-k+i];
-     }
-        //step2: Shieft the rest of the array to the right
-        //crucial: go backward so you don't lose data!
-        for(int i = n-k-1; i>=0; i--){
-            nums[i+k] = nums[i];
+     //step 1: Reverse the whole array
+     //[1,2,3,4,5] - [5,4,3,2,1]
+     reverse(nums,0,n-1);
 
-        }
-        //step3: put the temp element back at the start 
-        for(int i=0; i<k; i++){
-            nums[i] = temp[i];
-        }
+     //step2 : reverse the first k element
+     //[5,4,3,2,1]  - [4,5 ,3,2,1] (if k=2)
+     reverse(nums,0,k-1);
 
+     //step3: Reverse the rest
+     //[4,5,3,2,1] - [4,5,1,2,3
+     reverse(nums,k,n-1);
     
      }
+     //helper function the swap element
+     private void reverse(int[] nums,int start,int end){
+        while (start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+     
     }
+}
