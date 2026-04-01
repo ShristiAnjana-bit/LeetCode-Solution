732279aa6a01d54e7d328b32e9cb396a8f0c323c
ShristiAnjana-bit <Shristip263@gmail.com>
2026-04-01 21:50:41 +0530

    Time: 2 ms (99.28%), Space: 47.1 MB (66.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,23 +1,22 @@
 class Solution { // declares a class 
-    public int[] twoSum(int[] nums, int target) { // method called two sum
-        //int is the return type = array of integer(the two indices).
-        //(int[] nums, int target) = targets = num- array of integer given a problem
-        // target - the number we want to form by adding two elements
+    public int[] twoSum(int[] nums, int target) { 
 
-        for(int i = 0; i<nums.length;i++){  // int i = 0 -- start from index 0 , 
-                                            //i < nums.length --loop until the last element
-                                            // i++ -- increase i by 1 each time so this loop  
-                                            // i go through every index of the array
-        for(int j = i+1; j<nums.length;j++){  // start from the element after i.
-                                             // we don't start at 0 , because we don't want to use the same number twice
-                                             // j<nums.length loop until the end of the arry
-                                             // j++ move to the next index
-     if(nums[i]+ nums[j]== target){
-        return new int[] {i,j};
-     }
-        }                                    
+        //create memory (hashmap)
+        HashMap<Integer, Integer> map = new HashMap<>();
 
+        for(int i =0; i<nums.length; i++){
+            int complement = target-nums[i];
+            // the missing piece
+
+            //if we have seen the complement before return the indices
+            if(map.containsKey (complement)){
+                return new int[] {
+                    map.get(complement),i};
+                }
+                //otherwise , store the current number and its index
+                map.put(nums[i],i);
+            }
+            return new int[]{}; // return empty if no solution
         }
-        return new int[] {}; 
+    
     }
-}
