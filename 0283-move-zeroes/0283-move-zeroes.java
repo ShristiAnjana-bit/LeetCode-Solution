bedc5448bd541c29599e27d727bd29a67de6f4ef
ShristiAnjana-bit <Shristip263@gmail.com>
2026-04-03 01:17:11 +0530

    Time: 2 ms (90.91%), Space: 47.8 MB (47.6%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,22 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-       int j=0; //this the gatekeeper ..j will stand at first were we have to put non zeros
+       int j=0;
 
-       for(int i=0; i<nums.length;i++){ //loop through the array from starting
-       if(nums[i]!=0){
-    
-        nums[j] = nums[i]; //first ,put the person in the chair
-        j++; //then,move the 'next chair' pointer forward
+       for(int i =0; i<nums.length; i++){
+        if(nums[i]!=0){
+            nums[j] = nums[i];
+            j++;
+        }
        }
-       }
-
        while(j<nums.length){
         nums[j] =0;
         j++;
        }
-
-       
-
 
         
     }
