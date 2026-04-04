45dba6d4414bebad1fca46883d364be36ec872f9
ShristiAnjana-bit <Shristip263@gmail.com>
2026-04-04 11:54:36 +0530

    Time: 2 ms (22.88%), Space: 61.4 MB (47.62%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,38 +1,25 @@
 class Solution {
     public void rotate(int[] nums, int k) {
      int n= nums.length;
-     if(nums == null || nums.length == 0){
-        return ;  //if array is null(doesn't exist )and array is empty(doesn't have any element) return the value
-
-     }
-    //handle case where k>n
-     k=k%n;
-     if(k==0){
+     if(nums== null|| nums.length==0){
         return;
      }
-
-    //step 1: reverse the first k elemts
-    reverse(nums,0,n-1);
-
-    //step 2: reverse the rest elements
-    reverse(nums,0,k-1);
-
-    //step3: reverse the all elements
-    reverse(nums,k,n-1);
+     k=k%n;
+     if(k==0){
+        return ;
+    
+     }
+     reverse(nums,0,n-1);
+     reverse(nums,0,k-1);
+     reverse(nums,k,n-1);
     }
-
-    //helper function to swap the elements
-    private void reverse(int[]nums,int start,int end){
+     private void reverse(int[]nums,int start,int end){
         while(start<end){
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
         }
     }
-
-     
-        
-     
-    }
+}
