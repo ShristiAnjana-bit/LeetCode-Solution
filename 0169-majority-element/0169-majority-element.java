09c40e842f1dccbf3876a2f19ad84ef39711410a
Shristi Anjana <Shristip263@gmail.com>
2026-04-13 20:35:23 +0530

    Time: 1 ms (99.84%), Space: 55.5 MB (63.58%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,21 +1,24 @@
 class Solution {
     public int majorityElement(int[] nums) {
-      int n = nums.length;
+      int candidate =0;
+      int count =0;
 
-      HashMap<Integer,Integer> map = new HashMap<>();
-      for(int i= 0; i<nums.length; i++){
-        int num = nums[i]; //traversing through the arrAY
-
-        // Get current count(default to 0) and add1
-        map.put(num,map.getOrDefault(num,0)+1);
-
-        //check if this number is now the mejority
-        if(map.get(num)>n/2){
-            return num;
-        }
-
+      for(int num: nums){
+        //1.if the throne is empty ,take it!
+        if(count ==0){
+        candidate = num;
       }
-      return -1;
+
+      //2.if you are the same tribe,count goes up 
+      //if different, you both get knocked out(count goes down)
+      if(num == candidate){
+        count++;
+      }else{
+        count--;
+      }
 
     }
+    // the person left standing is the majority element
+    return candidate;
+}   
 }
