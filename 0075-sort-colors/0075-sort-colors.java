d14c4c658d1f5a2e5bbbf3d43b8c586b723779cd
Shristi Anjana <Shristip263@gmail.com>
2026-04-13 06:30:10 +0530

    Time: 0 ms (100%), Space: 43.5 MB (66.48%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,29 +1,28 @@
 class Solution {
     public void sortColors(int[] nums) {
-       int low =0;
-       int mid =0;
-       int high = nums.length-1;
+      int low = 0;
+      int mid =0;
+      int high = nums.length-1;
 
-       while(mid<=high){
-        if(nums[mid] == 0 ){ // swap mid with low
+      while(mid<=high){
+        if(nums[mid] == 0){
+            //swap mid with low
             int temp = nums[low];
-        nums[low] = nums[mid];
-        nums[mid] = temp;
-        mid++;
-        low++;
-        } else if(nums[mid] == 1){
-            //already in middle , just move on
+            nums[low] = nums[mid];
+            nums[mid] = temp;
             mid++;
-
-        } else { //nums[mid] == 2
-              //swap mid and high
-              int temp = nums[high];
-              nums[high] = nums[mid];
-              nums[mid] = temp;
-           high--;
-           //note we don't increment mid here becouse the swapped element needs to be checked
-           
+            low++;
+        }else if(nums[mid]== 1){
+            // mid value can be 1 so no need to swap anything
+            mid++;
+        }else{
+            int temp = nums[high];
+            nums[high] = nums[mid];
+            nums[mid] = temp;
+            high--;
+            
         }
-       }
+
+      }
 }
 }
