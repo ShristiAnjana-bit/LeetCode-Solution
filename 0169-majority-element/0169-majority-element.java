9a039fdcbb088676da612649748ffab34fc416c7
Shristi Anjana <Shristip263@gmail.com>
2026-04-14 10:10:16 +0530

    Time: 2 ms (70.57%), Space: 55.4 MB (68.9%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,24 +1,23 @@
 class Solution {
     public int majorityElement(int[] nums) {
-      int candidate =0;
-      int count =0;
+      int candidate = 0;
+      int count = 0;
 
       for(int num: nums){
-        //1.if the throne is empty ,take it!
         if(count ==0){
-        candidate = num;
-      }
+            candidate = num;
+        }
+      
 
-      //2.if you are the same tribe,count goes up 
-      //if different, you both get knocked out(count goes down)
+      
       if(num == candidate){
         count++;
       }else{
         count--;
       }
-
-    }
-    // the person left standing is the majority element
-    return candidate;
+      }
+      
+       return candidate;
+      
 }   
 }
