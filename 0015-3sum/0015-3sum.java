d3599b85a1ae0b2187912f55e04887d8b61dd844
Shristi Anjana <Shristip263@gmail.com>
2026-05-11 20:06:41 +0530

    Time: 31 ms (83.05%), Space: 59.1 MB (74.14%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,42 +1,41 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int n = nums.length;
-        List<List<Integer>> ans = new ArrayList<>();
+        List<List<Integer>> ans= new ArrayList<>();
+       //1.first we will sort the array
+       Arrays.sort(nums);
 
-        //sort the array
-        Arrays.sort(nums);
+       //2.we will iterate throught the array
+       for(int i =0; i<n; i++){
+        //for avoiding duplicates
+        if(i>0 && nums[i] == nums[i-1]) continue;
 
-        //interates with pointer
-        for(int i =0; i<n; i++){
-            //skip duplicates for the first elements
-            if(i >0 && nums[i] == nums[i-1]) continue;
-
-        
-        //3.two pointers
+        //now we will use two pointers
         int j = i+1;
         int k = n-1;
 
+        //now we will use while loop 
         while(j<k){
-            int sum = nums[i] +nums[j] + nums[k];
-            if(sum <0){
+            int sum = nums[i] +nums[j] +nums[k];
+            if(sum<0){
                 j++;
             }else if(sum>0){
                 k--;
             }else{
-                //found triplet
-                List<Integer>temp = Arrays.asList(nums[i],nums[j],nums[k]);
+                //found triplets
+                List<Integer> temp = Arrays.asList(nums[i],nums[j],nums[k]);
                 ans.add(temp);
 
-                //move pointers and skip duplicates
+                //move pointers and remove duplicates
                 j++;
                 k--;
                 while(j<k && nums[j] == nums[j-1]) j++;
                 while(j<k && nums[k] == nums[k+1]) k--;
+
             }
         }
-        
-        }
-        return ans;
+       }
+       return ans;
         
     }
     
