694c1ef7d47966d28c2ec21693f07c7764df2594
Shristi Anjana <Shristip263@gmail.com>
2026-05-12 12:00:21 +0530

    Time: 30 ms (87.96%), Space: 59.3 MB (45.42%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,42 +1,47 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int n = nums.length;
-        List<List<Integer>> ans= new ArrayList<>();
-       //1.first we will sort the array
+       int n = nums.length;
+       List<List<Integer>> ans = new ArrayList<>();
+       
+    
+
+       //sort array : this curcial for the two pointers and for removinh duplicates
        Arrays.sort(nums);
 
-       //2.we will iterate throught the array
-       for(int i =0; i<n; i++){
-        //for avoiding duplicates
+       //interate through the array
+       for(int i = 0; i<n; i++){
+        //for removing duplicates
         if(i>0 && nums[i] == nums[i-1]) continue;
 
-        //now we will use two pointers
-        int j = i+1;
-        int k = n-1;
+        //two pointers 
+        int j= i+1;
+        int k= n-1;
 
-        //now we will use while loop 
+        //while loop
         while(j<k){
-            int sum = nums[i] +nums[j] +nums[k];
+            int sum = nums[i] +nums[j] + nums[k];
             if(sum<0){
                 j++;
             }else if(sum>0){
                 k--;
             }else{
-                //found triplets
-                List<Integer> temp = Arrays.asList(nums[i],nums[j],nums[k]);
-                ans.add(temp);
+            //found triplets
+            List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+            ans.add(temp);
 
-                //move pointers and remove duplicates
-                j++;
-                k--;
-                while(j<k && nums[j] == nums[j-1]) j++;
-                while(j<k && nums[k] == nums[k+1]) k--;
+            //move pointers and skip duplicates
+            j++;
+            k--;
+
+            while(j<k && nums[j] == nums[j-1]) j++;
+            while(j<k && nums[k] == nums[k+1]) k--;
 
             }
         }
        }
-       return ans;
         
+       
+       return ans;
     }
     
 }
