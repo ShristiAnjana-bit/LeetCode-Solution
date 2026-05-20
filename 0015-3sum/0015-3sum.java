4df280d80520838cbeb0e33f4ba2cef6085a506a
Shristi Anjana <Shristip263@gmail.com>
2026-05-20 12:16:21 +0530

    Time: 31 ms (83.05%), Space: 59.5 MB (24.34%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,47 +1,40 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-       int n = nums.length;
-       List<List<Integer>> ans = new ArrayList<>();
-       
-    
+    public List<List<Integer>> threeSum(int[] nums){
+        int n= nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
 
-       //sort array : this curcial for the two pointers and for removinh duplicates
-       Arrays.sort(nums);
+        // 1. sort the array
+        Arrays.sort(nums);
 
-       //interate through the array
-       for(int i = 0; i<n; i++){
-        //for removing duplicates
+        for(int i =0; i<n; i++){
+        //2.skip the duplicates
         if(i>0 && nums[i] == nums[i-1]) continue;
 
-        //two pointers 
-        int j= i+1;
-        int k= n-1;
+        //3.two pointers
+        int j = i+1;
+        int k = n-1;
 
-        //while loop
         while(j<k){
-            int sum = nums[i] +nums[j] + nums[k];
-            if(sum<0){
+            int sum = nums[i] + nums[j] +nums[k];
+            if(sum <0){
                 j++;
-            }else if(sum>0){
+            }else if(sum >0){
                 k--;
             }else{
-            //found triplets
-            List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
-            ans.add(temp);
+                // Found a triplet
+                List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+                ans.add(temp);
 
-            //move pointers and skip duplicates
-            j++;
-            k--;
-
-            while(j<k && nums[j] == nums[j-1]) j++;
-            while(j<k && nums[k] == nums[k+1]) k--;
-
+                //move pointers and skip duplicates 
+                j++;
+                k--;
+                while(j<k && nums[j] == nums[j-1]) j++;
+                while(j<k && nums[k] == nums[k+1]) k--;
             }
+
         }
-       }
-        
-       
-       return ans;
+        }
+        return ans;
+    }
     }
     
-}
