7f78ecf7ef8f981b37960497f6299aa78b0ab843
Shristi Anjana <Shristip263@gmail.com>
2026-05-24 15:26:56 +0530

    Time: 1 ms (98.32%), Space: 47.6 MB (61.54%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,30 +1,52 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-     int n =matrix.length;
+     int n = matrix.length;
      int m = matrix[0].length;
+     int col0 =1; //extra variable for the first col
 
-     //step 1: Auxiliary arrays(the swithcs)
-     int[] row = new int[n];
-     int[] col = new int[m];
-
-     //marking ohase
+     //step 1: traverse the matrix and mark 1st row and 1st col
      for(int i =0; i<n; i++){
         for(int j=0; j<m; j++){
-            if(matrix[i][j]==0){
-                row[i] =1;
-                col[j] =1;
+            if(matrix[i][j] == 0){
+                //mark ith row
+                matrix[i][0] =0;
+            //mark jth col
+            if(j!=0)
+            matrix[0][j] =0;
+            else 
+            col0=0;
+
             }
         }
      }
-     //step 3: zeroing phase
-     for(int i =0; i<n; i++){
-        for(int j=0; j<m; j++){
-            //if row i OR colume j was marked
-            if(row[i] == 1|| col[j] ==1){
-                matrix[i][j] =0;
+     //step 2: update the inner matrix(1,1) to (n,m)
+     for(int i =1; i<n; i++){
+        for(int j=1; j<m; j++){
+            if(matrix[i][j] != 0){
+                //check if rwo and col are marked
+                if(matrix[i][0] == 0 || matrix[0][j]==0){
+                    matrix[i][j] =0;
+                }
             }
         }
+
      }
+     //step 3: update the first row(depend on matrix)
+     if(matrix[0][0] ==0){
+        for(int j=0; j<m; j++){
+            matrix[0][j] =0;
+
+        }
+     }
+     //step 4: update the first column(depend on col0)
+      if(col0 ==0){
+      for(int i=0; i<n; i++){
+        matrix[i][0] =0;
+      }
+
+      }
+
+
         }
         }
      
