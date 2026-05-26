f691560bc58d2e3ef3397ddccda1482768b85495
Shristi Anjana <Shristip263@gmail.com>
2026-05-27 04:58:46 +0530

    Time: 0 ms (100%), Space: 44.8 MB (31.25%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,36 +1,36 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-       int n= nums.length;
+       int n = nums.length;
+       int bp= -1; // break point
 
-       //step 1: find the breakpoint (the dip in mountain) 
-       int i = n-2;
-       while(i>=0 && nums[i]>=nums[i+1]){
-        i--;
+       //step 1: find breakpoint
+       for(int i =n-2; i>=0; i--){
+        if(nums[i] < nums[i+1]){
+            bp=i;
+            break;
+        }
        }
-       //if i becomes -1 , it means the array is sorted in decreasing order (eg 3,2,1)
-       //if i>=0 we found a breakpoint!
-     if(i >=0){
-        //step 2: Find the number "slightly greater" than nums[i] from the right
-        int j = n-1;
-        while(nums[j] <= nums[i]){
-            j--;
+       //step 2: breakpoint found, swap
+       if(bp != -1){
+        for(int i = n-1; i>bp; i--){
+            if(nums[i]>nums[bp]){
+                //swap
+                int temp = nums[i];
+                nums[i] = nums[bp];
+                nums[bp] = temp;
+                break;
+            }
         }
-        //swap the breakpoint with this slightly large number
-        swap(nums,i,j);
-
-     }
-     //step 3: reverse the tail (everything to the right of the breakpoint)
-     reverse(nums,i+1,n-1);
-    }
-    //helper method to swap elements
-    private void swap(int[] nums, int i,int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-    private void reverse(int[]nums,int start,int end){
-        while(start <end){
-            swap(nums,start++,end--);
-        }
+       }
+       //step 3: reverse from bp+1 to end
+       int left = bp+1;
+       int right = n-1;
+       while(left < right){
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+        left++;
+        right--;
+       }
     }
 }
