8b3953aacac3ef0999b3046add50d30c29569667
Shristi Anjana <Shristip263@gmail.com>
2026-05-26 08:35:36 +0530

    Time: 1 ms (98.3%), Space: 47.8 MB (25.45%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,45 +1,43 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
      int n = matrix.length;
-     int m =matrix[0].length;
-     int col0 = 1; //extra variable for column since first cell is shared by botrh row and colunm
+     int m= matrix[0].length;
+     int col0=1;
 
-     //step 1: use the markers
      for(int i =0; i<n; i++){
         for(int j =0; j<m; j++){
-            if(matrix[i][j]==0){
-                matrix[i][0] =0;
-                if(j!=0)
-                matrix[0][j] =0;
-                else
-                col0=0;
-            }
+            if(matrix[i][j] ==0){
+            matrix[i][0] =0;
+            if(j!=0)
+            matrix[0][j] =0;
+            else
+            col0=0;
         }
      }
-     //step 2: update the inner matrix
-     for(int i =1; i<n; i++){
+     }
+     for(int i =1; i <n; i++){
         for(int j =1; j<m; j++){
-            if(matrix[i][j]!=0){
-                if(matrix[i][0] ==0 || matrix[0][j] ==0){
-                    matrix[i][j] =0;
+            if(matrix[i][j] != 0){
+                if(matrix[i][0] ==0 || matrix[0][j] ==0 ){
+                
+                matrix[i][j] =0;
                 }
             }
         }
      }
-     //step 3: top row
-     if(matrix[0][0] == 0){
-        for(int j =0; j<m; j++){
-            matrix[0][j] =0;
-        }
+     //for the row 
+     if(matrix[0][0] ==0){
+     for(int j= 0; j<m; j++){
+        matrix[0][j] =0;
+     }
      }
 
-    //step 4: first col
-    if(col0 ==0){
-        for(int i =0; i<n; i++){
+     //for updating the first col
+     if(col0 == 0){
+        for(int i=0; i<n; i++){
             matrix[i][0] =0;
         }
-    }
-
+     }
         }
         }
      
