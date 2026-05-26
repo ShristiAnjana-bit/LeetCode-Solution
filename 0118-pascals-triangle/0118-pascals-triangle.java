a7ec3a990fdd8119b797209be030139990635533
Shristi Anjana <Shristip263@gmail.com>
2026-05-26 08:03:44 +0530

    Time: 0 ms (100%), Space: 43.2 MB (92.36%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,27 +1,23 @@
 class Solution {
     public  List<Integer> generateRow(int row) {
-        long ans =1; //use long to prevent overflow
-            List<Integer> ansRow = new ArrayList<>();
-            ansRow.add(1);  //the first element is always 1
+        long ans =1; // to avoid onverflow
+ List<Integer> ansRow = new ArrayList<>();
+ ansRow.add(1);
 
-            for(int col = 1; col<row; col++){
-                //logic : ans * (row.index -col_index)/ col_index
-                ans = ans * (row - col);
-                ans = ans/col;
-                ansRow.add((int) ans);
-            }
-            return ansRow;
-        
+ for(int col=1; col<row; col++){
+    ans = ans* (row-col);
+    ans = ans / col;
+    ansRow.add((int) ans);
+ }
+ return ansRow;
+
     }
-    //main funtion to gentrate the entire triangle up to row N
-    public  List<List<Integer>> generate(int numRows){
-        List<List<Integer>>result = new ArrayList<>();
-
+    public List<List<Integer>> generate(int numRows){
+        List<List<Integer>> result = new ArrayList<>();
         for(int i =1; i<=numRows; i++){
-            //gentrate row and col add it to final list
             result.add(generateRow(i));
         }
-        return result;
+         return result;
 
     }
     
