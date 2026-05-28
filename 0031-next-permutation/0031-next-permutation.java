4b01dff0bb6f37326c296c0297d7cbf55c23c71d
Shristi Anjana <Shristip263@gmail.com>
2026-05-28 08:27:10 +0530

    Time: 0 ms (100%), Space: 44.7 MB (49.68%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,20 +1,19 @@
 class Solution {
     public void nextPermutation(int[] nums) {
        int n = nums.length;
-       int bp= -1; // break point
+       int bp = -1; // breakpoint
 
-       //step 1: find breakpoint
-       for(int i =n-2; i>=0; i--){
+       for(int i = n-2; i>=0; i--){
         if(nums[i] < nums[i+1]){
-            bp=i;
+            bp =i;
             break;
         }
        }
-       //step 2: breakpoint found, swap
-       if(bp != -1){
+       // step 2 : if break point found : find next genreater
+       if(bp !=-1){
         for(int i = n-1; i>bp; i--){
-            if(nums[i]>nums[bp]){
-                //swap
+            if(nums[i] > nums[bp]){
+                // swap
                 int temp = nums[i];
                 nums[i] = nums[bp];
                 nums[bp] = temp;
@@ -22,13 +21,13 @@ class Solution {
             }
         }
        }
-       //step 3: reverse from bp+1 to end
-       int left = bp+1;
+       //step 3 rverse the ARRAY FROM BP +1 TO END
+       int left = bp +1;
        int right = n-1;
-       while(left < right){
+       while(left<right){
         int temp = nums[left];
         nums[left] = nums[right];
-        nums[right] = temp;
+        nums[right] =temp;
         left++;
         right--;
        }
