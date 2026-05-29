bc2a4a57169de46897303eadb16e58e6aa288c5d
Shristi Anjana <Shristip263@gmail.com>
2026-05-29 16:09:27 +0530

    Time: 0 ms (100%), Space: 43.6 MB (82.38%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -2,22 +2,25 @@ class Solution {
     public void rotate(int[][] matrix) {
        int n = matrix.length;
 
-       //step 1: Transpose (Swap element acrosss the diagonal) 
+       //step 1 : transpose -only upper tirangle(j starts at i+1)
        for(int i =0; i<n; i++){
-        for(int j=i; j<n; j++){
-            int temp = matrix[i][j];
-            matrix[i][j] = matrix[j][i];
-            matrix[j][i] = temp;
+        for(int j= i+1; j<n; j++){
+           int temp = matrix[i][j];
+           matrix[i][j] = matrix[j][i];
+           matrix[j][i] = temp;
         }
        }
-       //step 2: reverse each row 
-       for(int i =0; i<n; i++){
-        //standard two-pointer reverse logic for each row
-        for(int j= 0; j<n/2; j++ ){
-            int temp = matrix[i][j];
-            matrix[i][j] = matrix[i][n-1-j];
-            matrix [i][n-1-j] = temp;
+      // step 2: reverse each row
+      for(int i=0; i<n; i++){
+        int left =0, right=n-1;
+        while(left<right){
+            int temp = matrix[i][left];
+            matrix[i][left]= matrix[i][right];
+            matrix[i][right] = temp;
+            left++;
+            right--;
         }
-       }
+      }
+
     }
 }
