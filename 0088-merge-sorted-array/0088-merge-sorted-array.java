f8bf94f9eafbaec5d0cd1070cc6db0aa1f3e4c9f
Shristi Anjana <Shristip263@gmail.com>
2026-05-31 10:41:51 +0530

    Time: 0 ms (100%), Space: 43.4 MB (96.76%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,23 +1,23 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int p1= m-1; //last real elment in num1
-        int p2 =n-1; //last element in num2
-        int p = m + n -1; //last slot in nums1
+        int p1= m-1; //last real elemnt in num1
+        int p2= n-1; //last element in num2
+        int p = m+n-1; //last slot is num1
 
         //stop only when nums2 is fully placed
         while(p2 >= 0){
-            //if nums1 still has element AND its current is larger
-            if(p1 >= 0 && nums1[p1]>nums2[p2]){
+            //if nums1 still has element AND its current is 
+            if(p1 >= 0 && nums1[p1]> nums2[p2]){
                 nums1[p] =nums1[p1];
                 p1--;
             }else{
-                //either nums1 is exhausted or nums2[p2] is larger/equal
-                nums1[p] =nums2[p2];
+                //either nums1 is exhausted or nums 2[p2] is larger/equal
+                nums1[p] = nums2[p2];
                 p2--;
             }
             p--;
         }
-       // if p2<0, num2 done -- nums/remainder is already in place
+        //if p2<0 , nums done - nums 1 remainder is already inplace
         
     }
 }
