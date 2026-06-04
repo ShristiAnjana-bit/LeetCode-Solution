709a29b4c47f60c17d0bd79b9d08dd7a211901fc
Shristi Anjana <Shristip263@gmail.com>
2026-06-04 06:54:22 +0530

    Time: 1 ms (21.54%), Space: 48.9 MB (5.41%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,17 +1,27 @@
 class Solution {
     public void reverseString(char[] s) {
-        int left =0;
-        int right= s.length-1;
-
-        while(left<right){
-            //swap
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-
-            //move pointers
-            left++;
-            right--;
-        }
+        // we call a healper function and pass the starting boundaries
+        solve(s, 0, s.length-1);
     }
+    //helper funcation to track the left and right pointers
+    private void solve(char[] s,int left,int right){
+        //base case:if pointers cross or meet , stop recursion
+        if(left>=right){
+            return;
+
+        }
+        //process: swap characters at left and right
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
+        //Recursive call: move pointers closer and call again
+        solve(s,left+1 , right-1);
+
+    }
+        
+        
+
+        
+    
 }
