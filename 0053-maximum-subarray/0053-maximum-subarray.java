547bbf021cf52e775ed731ba9a4a04f8ccac9fa6
Shristi Anjana <Shristip263@gmail.com>
2026-06-11 10:46:09 +0530

    Time: 1 ms (99.95%), Space: 77.1 MB (64.77%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,15 +3,16 @@ class Solution {
     int currentSum = nums[0];
     int maxSum = nums[0];
 
-    for(int i = 1; i < nums.length; i++){
+    for (int i =1; i<nums.length; i++){
         if(currentSum < 0){
             currentSum = nums[i];
         }else{
             currentSum += nums[i];
-
         }
         maxSum = Math.max(maxSum, currentSum);
     }
-    return maxSum;
+
+ return maxSum;
     }
+
 }
