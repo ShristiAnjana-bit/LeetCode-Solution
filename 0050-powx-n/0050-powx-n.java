9b8904ed40e1c24e6d703151c138030f425b342a
Shristi Anjana <Shristip263@gmail.com>
2026-06-16 17:43:57 +0530

    Time: 0 ms (100%), Space: 48.4 MB (10.8%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,26 @@
+//class containing power calculation logic 
+class Solution {
+    //helper method to calculate power using recursion
+    public double myPow(double x, int n) {
+        long N = n; //prevent overflow
+
+        if(N < 0){
+            x = 1 /x;
+            N = -N;
+        }
+        double ans = 1;
+
+        while(N>0){
+            //if exponent is odd
+            if(N % 2 == 1){
+                ans *= x;
+            }
+             //sq the base
+             x *= x;
+
+             //halve the exponent
+             N /= 2;
+        }
+        return ans;
+    }
+}
