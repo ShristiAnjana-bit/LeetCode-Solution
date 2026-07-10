c5ffffe971db0978e3b3bdc60c9ead142877cfd7
Shristi Anjana <Shristip263@gmail.com>
2026-07-10 21:43:01 +0530

    Time: 2 ms (70.26%), Space: 55.3 MB (69.21%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,33 @@
 class Solution {
     public int majorityElement(int[] nums) {
-     int candidate = 0;
-     int count = 0;
+     int candidate =0, count = 0;
 
      for(int num : nums){
         if(count == 0){
             candidate = num;
+            
         }
         if(num == candidate){
             count++;
         }else{
-        count--;
+            count--;
         }
+
      }
      return candidate;
      
 }   
 }
+
+//Majority element > n/2
+
+//Different elements ek dusre ko cancel kar sakte hain
+
+//Agar count 0 ho jaye
+//→ naya candidate choose karo
+
+//Same element mile
+//→ count++
+
+//Different mile
+//→ count--
