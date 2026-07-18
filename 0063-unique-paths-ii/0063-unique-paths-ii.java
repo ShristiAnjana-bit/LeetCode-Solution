dc5023507a3dbae235f11e75e99adc13f02b7dba
Shristi Anjana <Shristip263@gmail.com>
2026-07-18 10:50:04 +0530

    Time: 0 ms (100%), Space: 43.7 MB (27.14%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -4,37 +4,51 @@ class Solution {
          int m = obstacleGrid.length;
          int n = obstacleGrid[0].length;
 
-         int[] prev = new int[n];
+    int[][] dp = new int[m][n];
 
-         for(int i = m-1; i >= 0; i--){
-             int[] curr = new int[n];
+    for (int[] row: dp){
+      Arrays.fill (row,-1);
+    
+    }
+    return solve(0,0,obstacleGrid, dp);
+    
+}
+    public int solve(int i, int j, int[][] grid ,int[][] dp){
 
-        for(int j = n-1; j >=0; j--){
+      int m = grid.length;
+      int n = grid[0].length;
 
-            if(obstacleGrid[i][j] == 1){
-                curr[j] =0;
-            }
+      //base case 1: out of bundary 
+     if(i >= m || j >=n){
+        return 0;
+     }
 
-            else if(i == m-1 && j == n-1){
-                curr[j] = 1;
+     //base case 2: obstacle
+     if(grid[i][j] == 1){
+        return 0;
 
-            }
-            else{
-                int right =0;
-                int down =0;
+     }
 
-                if(j+1<n)
-                right = curr[j+1];
+     //base case 3: Destination
+     if(i == m-1 && j == n-1){
+        return 1;
+     }
+     //memoization 
+     if(dp[i][j] != -1){
+        return dp[i][j];
+     }
 
-                if(i+1<m)
-                down = prev[j];
+     //recursive calls
+     int right = solve(i , j+1, grid, dp);
+     int down = solve(i+1, j, grid, dp);
 
-                curr[j] = right + down;
-            }
-        }
-        prev = curr;
+     //store and return 
+     dp[i][j] = right + down;
 
-         }
-   return prev[0];
+     return dp[i][j];
+
     }
 }
+
+      
+    
